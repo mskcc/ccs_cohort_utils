99f0499abf5d5f545e0832993508d354dad5f62c
Anne Marie Noronha <noronhaa@mskcc.org>
2024-02-26 12:45:09 -0500

    surface schema properties to the package level

--- tests/validate_schema.py
+++ tests/validate_schema.py
@@ -1,7 +1,8 @@
 import json
 import jsonschema
 import unittest
-from cohort_utils.schema import *
+#from cohort_utils.schema import *
+import cohort_utils
 import sys
 
 BAM_COMPLETE    = "./data/json_schema/bam-complete.example.json"
@@ -13,19 +14,19 @@ class validateschema(unittest.TestCase):
     def test_bam_complete_json(self):
         with open(BAM_COMPLETE,'r') as fh:
             instance = json.load(fh)
-        jsonschema.validators.validate(instance=instance, schema=BAM_COMPLETE_JSON_SCHEMA)
+        jsonschema.validators.validate(instance=instance, schema=cohort_utils.schema.BAM_COMPLETE_JSON_SCHEMA)
     def test_maf_complete_json(self):
         with open(MAF_COMPLETE,'r') as fh:
             instance = json.load(fh)
-        jsonschema.validators.validate(instance=instance, schema=MAF_COMPLETE_JSON_SCHEMA)
+        jsonschema.validators.validate(instance=instance, schema=cohort_utils.schema.MAF_COMPLETE_JSON_SCHEMA)
     def test_qc_complete_json(self):
         with open(QC_COMPLETE,'r') as fh:
             instance = json.load(fh)
-        jsonschema.validators.validate(instance=instance, schema=QC_COMPLETE_JSON_SCHEMA)
+        jsonschema.validators.validate(instance=instance, schema=cohort_utils.schema.QC_COMPLETE_JSON_SCHEMA)
     def test_cohort_complete_json(self):
         with open(COHORT_COMPLETE,'r') as fh:
             instance = json.load(fh)
-        jsonschema.validators.validate(instance=instance, schema=COHORT_COMPLETE_JSON_SCHEMA)
+        jsonschema.validators.validate(instance=instance, schema=cohort_utils.schema.COHORT_COMPLETE_JSON_SCHEMA)
 
 if __name__ == "__main__":
     unittest.main()
