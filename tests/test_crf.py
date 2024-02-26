acc6723b55e49827a931ec5d44a8932225c4356d
Anne Marie Noronha <noronhaa@mskcc.org>
2024-02-26 12:45:47 -0500

    update crf test

--- tests/test_crf.py
+++ tests/test_crf.py
@@ -3,7 +3,6 @@ from cohort_utils.model import Pairing
 import unittest
 from cohort_utils.parsers.crj import CRJ_Handler
 import cohort_utils
-#import cohort_utils.schema.CRJ_JSON_SCHEMA as CRJ_JSON_SCHEMA
 
 COHORTFILE  = "./data/COHORT1.cohort.txt"
 COHORTFILE2 = "./data/COHORT2.cohort.txt"
@@ -29,7 +28,7 @@ class TestCRF(unittest.TestCase):
         crf_handle = parsers.crf.CRF_Handler(crf=COHORTFILE)
         my_cohort = crf_handle.to_cohort()
         my_crj = my_cohort.to_json()
-        my_crj_handler = CRJ_Handler(my_crj,cohort_utils.schema.CRJ_JSON_SCHEMA)
+        my_crj_handler = CRJ_Handler(my_crj,cohort_utils.schema.COHORT_REQUEST_JSON_SCHEMA)
         print(my_crj)
         assert len(my_crj_handler) == 2
         assert len(my_crj["manifest"]) == 2
