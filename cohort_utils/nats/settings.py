8a049a80725ee47c30c1b439d27ace2176174ae9
Anne Marie Noronha <anoronh4@users.noreply.github.com>
2026-03-20 12:05:51 -0400

    update output location of embargo dates record in local and dev profiles

--- cohort_utils/nats/settings.py
+++ cohort_utils/nats/settings.py
@@ -12,9 +12,12 @@ if METADB_PROFILE not in ["dev","prod","local"]:
     
     raise ValueError("invalid NATS_PROFILE value")
 
-if METADB_PROFILE in ["dev", "local"]:
+if METADB_PROFILE in ["local"]:
     CRF_OUTPUT_DIR = "."
-    EMBARGO_DATE_RECORD = "./embargo_dates.txt"
+    EMBARGO_DATE_RECORD = "./embargo_dates_local.txt"
+elif METADB_PROFILE in ["dev"]:
+    CRF_OUTPUT_DIR = "."
+    EMBARGO_DATE_RECORD = "./embargo_dates_dev.txt"
 else:
     CRF_OUTPUT_DIR = "/data1/core006/ccs_pipelines/tempo/wes_repo/Results/v2.1.x/cohort_level/"
     EMBARGO_DATE_RECORD = "/data1/core006/ccs_pipelines/tempo/tempodeliver_wes_repo_v2.0.x/stage_runs/deliver_cohorts_v2.1.x/embargo_dates.txt"
